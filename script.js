40ac425326c8e8ef88c059a7dcaba9f3bdc8a4d9
SiceeGroup <gaetano.perla@gmail.com>
2025-05-30 02:19:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
      contactUsBtn.addEventListener('click', () => {
         // Rimuovi la riga dell'alert e aggiungi il reindirizzamento
-        window.location.href = 'http://wwww.siceegroup.it';
+        window.location.href = 'https://form.jotform.com/251487413706358';
         // Se vuoi aprire in una nuova scheda, usa:
         // window.open('http://www.siceegroup.it', '_blank');
     });
